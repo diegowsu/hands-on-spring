0e9bf89c396970c93a10469581dfd26a7bc8e3df
dgalhardi <diegowsu@gmail.com>
2021-02-22 20:12:31 -0300

    Alteracoes

--- projectthree/src/main/java/io/gitthub/sampleapi/controller/ClienteController.java
+++ projectthree/src/main/java/io/gitthub/sampleapi/controller/ClienteController.java
@@ -4,10 +4,12 @@ import io.gitthub.sampleapi.Repository.ClienteRepository;
 import io.gitthub.sampleapi.model.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Controller
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
